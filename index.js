fd8fa34a5a6dd4294c987148cb28de67cadf7345
Michael Friedman <minorpianokeys@gmail.com>
2023-08-16 22:26:27 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.toUpperCase() === name.toUpperCase();
+    })
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.toUpperCase().substring(0, name.length) === name.toUpperCase();
+    }) 
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver["name"] === name;
+    })
+}
